f39560f9041b8958c455b9d2e32b96cf6c5d7dd2
guru <punugupati4@gmail.com>
2016-05-08 13:44:56 +0530

    @guru main java class is added

---
+++ DemoModule/src/SampleClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by GURU on 5/8/2016.
+ */
+public class SampleClass {
+    public static void main(String[] s)
+    {
+        System.out.println("main class demo");
+
+        
+
+
+    }
+
+}
